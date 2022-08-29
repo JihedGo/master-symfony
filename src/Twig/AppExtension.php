fa93915deedb486f241faabc5d99c1a8e1d2cc04
webdev <jihed.gouay@gmail.com>
2022-08-29 16:51:20 +0100

    part1 :twig global varible

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -3,14 +3,27 @@
 namespace App\Twig;
 
 use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
 use Twig\TwigFilter;
 
-class AppExtension extends AbstractExtension
+class AppExtension extends AbstractExtension implements GlobalsInterface
 {
+    private $locale;
+    public function __construct(string $locale)
+    {
+        $this->locale = $locale;
+    }
     public function getFilters()
     {
         return [
             new TwigFilter('price', [$this, 'priceFilter'])
+        ];
+    }
+
+    public function getGlobals(): array
+    {
+        return [
+            'locale' => $this->locale
         ];
     }
 
