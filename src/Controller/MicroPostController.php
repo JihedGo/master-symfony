b754040cf38e977a061f7ce33e89c4a5ec879f26
webdev <jihed.gouay@gmail.com>
2022-08-30 13:07:14 +0100

    change template & repo

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\MicroPost;
+use App\Form\MicroPostType;
 use App\Repository\MicroPostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,6 +23,26 @@ class MicroPostController extends AbstractController
     {
         return $this->render('micro-post/index.html.twig', [
             'posts' => $repo->findAll()
+        ]);
+    }
+
+
+    /**
+     * @Route("/add", name="micro_post_add")
+     */
+    public function add(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $microPost = new MicroPost;
+        $microPost->setTime(new \DateTime());
+        $form      = $this->createForm(MicroPostType::class, $microPost);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($microPost);
+            $entityManager->flush();
+            return $this->redirectToRoute('micro_post_index');
+        }
+        return $this->render('micro-post/add.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
