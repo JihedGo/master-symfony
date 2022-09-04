eeeccf7439c13f18f2f95440ac062932592c509d
webdev <jihed.gouay@gmail.com>
2022-09-04 14:47:39 +0100

    fixing the problem of user can follow himself

--- src/Controller/FollowingController.php
+++ src/Controller/FollowingController.php
@@ -21,8 +21,11 @@ class FollowingController extends AbstractController
     {
         /** @var User $currentUser */
         $currentUser = $this->getUser();
-        $currentUser->getFollowing()->add($userToFollow);
-        $this->getDoctrine()->getManager()->flush();
+        if ($userToFollow->getId() !== $currentUser->getId()) {
+
+            $currentUser->getFollowing()->add($userToFollow);
+            $this->getDoctrine()->getManager()->flush();
+        }
         return $this->redirectToRoute('micro_post_user', ['username' => $userToFollow->getUsername()]);
     }
 
